de05d648c59f57f254df86d82378f0dac2b4fa3f
catcodey <103652349+catcodey@users.noreply.github.com>
2022-12-27 19:13:37 +0530

    deletion

--- project.cpp
+++ project.cpp
@@ -119,3 +119,132 @@ void login(){
         //menu();
         }
 }
+
+
+//DELETION
+
+#include <iostream>
+#include<fstream>
+#include<sstream>
+#include<string.h>
+#include<vector>
+using namespace std;
+void adddata();
+int main()
+{
+    adddata();
+    string str,name1;
+    int rollno,num,i;
+    //string word2;
+    vector<string>row;
+    
+    /*obj.open("data.txt",ios::out);
+    cin>>name>>name1;
+    obj<<name<<" "<<name1<<"\n";
+    cin>>rollno;
+    obj<<rollno;
+    obj.close();*/
+    fstream obj,obj1;
+    obj.open("data.txt",ios::out|ios::in);
+    obj1.open("data1.txt",ios::in|ios::out);
+    /*while(!obj.eof()){
+        getline(obj,word);
+        //cout<<typeid(str).name();
+        cout<<word<<"\n";
+        
+    }*/
+    string word;
+    cin>>num;
+    while(getline(obj,str)){
+        row.clear();
+        stringstream obj(str);
+        while(getline(obj,word,',')){
+            row.push_back(word);
+        }
+        int word1=stoi(row[0]);
+        if(num!=word1){
+            for(i=0;i<row.size();i++){
+                obj1<<row[i]<<" ";
+            }
+            obj1<<"\n";
+        }
+        rename("data1.txt","data.txt");
+    }
+    obj.close();
+    obj1.close();
+    string word2,name,branch;
+    int rollNumber;
+    obj.open("data.txt",ios::in|ios::out);
+    while(!obj.eof()){
+       // getline(obj, rollNumber, ",");
+       // getline(obj, name, ',');
+        
+       // getline(obj, branch, ',');
+       
+        //cout<<typeid(str).name();
+        getline(obj,word2);
+        //cout << rollNumber << " \t " << name << branch <<endl;
+        cout<<word2;
+    }
+
+    return 0;
+}
+void adddata(){
+    fstream file("data.txt",ios::out| ios::app);
+   //check of the file is open or not
+    if(!file)
+    {
+        cout<<"failed to open the file";
+    }
+    else{
+        cout<<"enter the data";
+    }
+    //declare variables
+    string name,college,branch;
+    int rollno,attendance,physics,che,maths;
+    //enter the  values
+    cout<<"\n enter the rollno : ";
+    cin>>rollno;
+    file<<rollno<<",";
+    cout <<"\n enter the name of the student : ";
+    cin>>name;
+    file<<name<<",";
+    cout<<"\n enter the college name: ";
+    cin>>college;
+    file<<college<<",";
+
+cout<<"\n enter the branch: ";
+    cin>>branch;
+    file<<branch<<",";
+    
+    cout<<"\n enter the attendance percentage: "; 
+    cin>>attendance;
+    file<<attendance<<",";
+    cout<<"\n enter the physics mark: ";
+    cin>>physics;
+    file<<physics<<",";
+    cout<<"\n enter the maths mark: ";
+    cin>>maths;
+    file<<maths<<",";
+    cout<<"\n enter the chemistry mark: ";
+    cin>>che;
+    file<<che<<",";
+    file<<"\n";
+    //close file
+    file.close();
+    //options to add the missed out data
+    cout<<"\n add missed out data? (YES/NOOO) : ";
+    string option;
+    cin>>option;
+    if(option=="YES")
+    {//back to adddata function
+        adddata();
+    }
+    else if(option=="NOOO")
+    {//back to the main menu
+       // admindata();
+    }
+    
+    
+    
+}
